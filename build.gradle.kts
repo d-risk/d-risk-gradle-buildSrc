c08717339ccec8f5ac6092857ff5c74271b2b135
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-10-04 14:04:31 +0800

    add dependency on gradle-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,10 @@ repositories {
     jcenter()
 }
 
+dependencies {
+    implementation(kotlin("gradle-plugin"))
+}
+
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
     gradleVersion = "5.6.2"
