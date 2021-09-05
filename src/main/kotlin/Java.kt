9ba98abd4edd96f306a70d84b9f058db0b149859
Christopher Zhong <christopher.zhong@d-risk.tech>
2021-09-05 09:29:27 +0800

    Change JVM target version from 1.8 to 11 (1.11)

--- src/main/kotlin/Java.kt
+++ src/main/kotlin/Java.kt
@@ -4,4 +4,4 @@ import Versions.JAVA
 import org.gradle.api.plugins.JavaPluginExtension
 
 val JavaPluginExtension.JAVA_VERSION get() = JAVA
-const val JVM_TARGET: String = "1.8"
+const val JVM_TARGET: String = "11"
